e1decdac4c01fa0cfda5fab0f9b6d63bcd9b424e
krepinnegga <102814848+krepinnegga@users.noreply.github.com>
2024-01-14 17:25:24 +0000

    Header complete

--- Screens/HomeScreen/components/Header/Header.jsx
+++ Screens/HomeScreen/components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
 import React from 'react'
 import { useUser } from '@clerk/clerk-expo'
 import Colors from '../../../../Utils/Colors';
+import { Feather } from '@expo/vector-icons';
+
 
 
 const Header = () => {
@@ -9,16 +11,39 @@ const Header = () => {
 
  return user && (
     <View style={styles.container}>
-       <View>
-           <Image source={{uri:user?.imageUrl}}
-             style={styles.userImage}
-           />
+      {/* Profile Section */}
+        <View style={styles.profileMainContainer}>
+            <View style={styles.profileContainer}>
+                <Image source={{uri:user?.imageUrl}}
+                    style={styles.userImage}
+                />
 
-           <View style={{}}>
-               <Text>Welcome</Text>
-               <Text>{user?.firstName}</Text>
-           </View>
+                <View>
+                    <Text style={{color:Colors.WHITE}}>
+                        Welcome,
+                    </Text>
+                    <Text style={{color:Colors.WHITE, fontSize:20}}>
+                        {user?.firstName}
+                    </Text>
+                </View>
+            </View>
+           <Feather name="bookmark" size={24} color="white" />
        </View>
+      {/* Profile Section */}
+
+      {/* Search Bar Section */}
+         <View style={styles.searchContainer}>
+            <TextInput placeholder='search'
+             style={styles.textInput}
+             />
+             <Feather 
+                name="search" 
+                size={24} 
+                color={Colors.PRIMARY} 
+                style={styles.searchBtn}
+             />
+         </View>
+      {/* Search Bar Section */}
     </View>
   )
 }
@@ -29,11 +54,45 @@ const styles = StyleSheet.create({
     container:{
         padding: 20,
         paddingTop: 40,
-        backgroundColor: Colors.PRIMARY
+        backgroundColor: Colors.PRIMARY,
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25
+    },
+    profileMainContainer:{
+        display:"flex",
+       flexDirection:"row",
+       alignItems:"center",
+       justifyContent:"space-between"
+    },
+    profileContainer:{
+       display:"flex",
+       flexDirection:"row",
+       alignItems:"center",
+       gap:10
     },
     userImage:{
         width: 50,
-        height: 45,
-        borderRadius: 99,   
+        height: 50,
+        borderRadius: 100,   
     },
+    textInput:{
+        padding:7,
+        paddingHorizontal:15,
+        backgroundColor:Colors.WHITE,
+        borderRadius:8,
+        width: "85%",
+        fontSize: 16
+    },
+    searchContainer:{
+        marginTop: 15,
+        display:'flex',
+        flexDirection:"row",
+        gap: 10,
+        marginBottom:10
+    },
+    searchBtn:{
+        backgroundColor:Colors.WHITE,
+        padding:10,
+        borderRadius:8,
+    }
 })
